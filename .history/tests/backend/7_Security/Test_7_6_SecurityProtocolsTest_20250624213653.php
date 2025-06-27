ee81b18ed78959d155fedf9aaf81a88c73d3ed00
Graeme Lowe <graeme.lowe@happinessbydesign.co.nz>
2025-06-27 16:27:15 +1000

    Test 7.6

---
+++ .history/tests/backend/7_Security/Test_7_6_SecurityProtocolsTest_20250624213653.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../../../model/functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * # Traceability
+ */
+class Test_7_6_SecurityProtocolsTest extends TestCase
+{
+}
