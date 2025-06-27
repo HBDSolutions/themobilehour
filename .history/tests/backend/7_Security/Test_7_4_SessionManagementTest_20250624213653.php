7097ae84f8728e4893f12e4a0c18a3f5acabe1c1
Graeme Lowe <graeme.lowe@happinessbydesign.co.nz>
2025-06-27 16:23:27 +1000

    Test 7.4

---
+++ .history/tests/backend/7_Security/Test_7_4_SessionManagementTest_20250624213653.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../../../model/functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * # Traceability
+ */
+class Test_7_4_SessionManagementTest extends TestCase
+{
+}
