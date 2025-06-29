30bb71b315302c905cb6a096b5891cc3bbe3979e
Graeme Lowe <graeme.lowe@happinessbydesign.co.nz>
2025-06-29 13:29:08 +1000

    Customer admin MVC compliant

---
+++ controller/managecustomers.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+require_once($_SERVER['DOCUMENT_ROOT'] . "/themobilehour/model/database.php");
+include($_SERVER['DOCUMENT_ROOT'] . "/themobilehour/view/add_customer.php");
+exit();
+?>
