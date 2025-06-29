5636e8ac5344b3e620580d8f7142e32dccc3ff11
Graeme Lowe <graeme.lowe@happinessbydesign.co.nz>
2025-06-29 15:04:25 +1000

    Deleting history since updates committed regularly

--- .history/controller/managecustomers_20250629132603.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require_once($_SERVER['DOCUMENT_ROOT'] . "/themobilehour/model/database.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/themobilehour/view/add_customer.php");
-exit();
-?>
